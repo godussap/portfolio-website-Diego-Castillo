19143b94c6cd73025594b50ce5551a04a69f2c63
Godussap <sebastiancastillo0312@gmail.com>
2024-10-10 12:36:03 -0500

    Add files via upload

---
+++ Professional Web-Site - Copy/app.js
@@ -0,0 +1,40 @@
+
+Vue.createApp({
+    data(){
+      return{
+        content:'Hello, Im Diego Castillo and i am inviting you to come at look at my personal website.',
+        content1:'Let me tell you something about myself. I am currently working as an IT but my goal is to become a professional programmer and developer.',
+        welcome:'Welcome to my personal page',
+        backgroundImage:'url(../image/background)',
+        links: [
+          {text:'Home' , url:'index.html'}, 
+          {text:'About', url:'about.html'},
+          {text:'Contact', url:'contact.htm'}
+        ],
+      };
+    },
+  
+}).mount('body');
+
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+
+ 
+  
+  
+  
+  
+  
+  
+  
+  
+  
